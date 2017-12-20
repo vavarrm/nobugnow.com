b8a6b73c60e82d3721a31a98042fdfeb00723a30
tryion <vavarrm@gmail.com>
2017-12-20 16:10:02 +0700

    11

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -7,9 +7,7 @@ class Api extends CI_Controller {
 	
 	public function __construct() 
 	{
-		parent::__construct();
-		var_dump($_SERVER);
-		exit;		
+		parent::__construct();	
 		$this->request = json_decode(trim(file_get_contents('php://input'), 'r'), true);
 		$this->load->model('User_Model', 'user');
 		$this->load->model('Log_Model', 'myLog');
